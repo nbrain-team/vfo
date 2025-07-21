3915a743e11142701e6b741a7e85a632229ce5fe
Danny DeMichele <danny@nbrain.com>
2025-07-21 08:43:10 -0700

    fix: Purge final Tailwind remnants

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
